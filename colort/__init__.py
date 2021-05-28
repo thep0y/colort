188250248119244076d4c212c1294717192b251b
thep0y <thepoy@163.com>
2021-05-28 08:53:57 +0800

    update version to 0.3

--- colort/__init__.py
+++ colort/__init__.py
@@ -4,10 +4,10 @@
 # @Email: thepoy@163.com
 # @File Name: __init__.py (c) 2021
 # @Created:  2021-05-27 09:34:57
-# @Modified: 2021-05-27 09:38:50
+# @Modified: 2021-05-28 08:53:46
 
 from colort.colort import DisplayStyle
 
-__version__ = "0.2"
+__version__ = "0.3"
 
 display_style = DisplayStyle()
